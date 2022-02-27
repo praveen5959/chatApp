ecb54e308af714ca5a7da42abbec98edc628f1df
praveen5959 <praveenbhat98@gmail.com>
2022-02-27 11:14:25 +0530

    Login and Sign UP UI Created

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,7 +1,64 @@
 import React from "react";
+import {
+  Container,
+  Box,
+  Text,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+} from "@chakra-ui/react";
+
+import Login from "../components/Authentication/Loign";
+import SignUp from "../components/Authentication/SignUp";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <Container maxWidth="xl" centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        p={3}
+        bg={"#404040"}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="4xl" fontFamily="Work sans" color="white">
+          Chat-App
+        </Text>
+      </Box>
+      <Box
+        bg={"#404040"}
+        w="100%"
+        p={4}
+        color="white"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Tabs variant="soft-rounded">
+          <TabList mb="1em">
+            <Tab width="50%" color="white">
+              Login
+            </Tab>
+            <Tab width="50%" color="white">
+              Sign Up
+            </Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <SignUp />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
 };
 
 export default HomePage;
